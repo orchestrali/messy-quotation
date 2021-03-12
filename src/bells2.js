02e4c95c6687e61b276a75d537ac248f1c7a6ac3
Glitch (transparent-messy-quotation) <none>
2021-03-12 01:50:16 +0000

    🏎📠 Updated with Glitch

---
+++ src/bells2.js
@@ -0,0 +1,126 @@
+
+module.exports = [
+  {
+    bell: "g4",
+    url: "bell55.wav?v=1597702196155",
+    type: "mabel"
+  },
+  {
+    bell: "a4",
+    url: "bell57.wav?v=1597702192059",
+    type: "mabel"
+  },
+  {
+    bell: "b4",
+    url: "bell59.wav?v=1597702186532",
+    type: "mabel"
+  },
+  {
+    bell: "c5",
+    url: "bell60.wav?v=1597702182176",
+    type: "mabel"
+  },
+  {
+    bell: "d5",
+    url: "bell62.wav?v=1597702179218",
+    type: "mabel"
+  },
+  {
+    bell: "e5",
+    url: "bell64.wav?v=1597702174624",
+    type: "mabel"
+  },
+  {
+    bell: "v5",
+    url: "bell66.wav?v=1597802587244",
+    type: "mabel"
+  },
+  {
+    bell: "g5",
+    url: "bell67.wav?v=1597802597437",
+    type: "mabel"
+  },
+  {
+    bell: "a5",
+    url: "bell69.wav?v=1597802675558",
+    type: "mabel"
+  },
+  {
+    bell: "b5",
+    url: "bell71.wav?v=1597802688929",
+    type: "mabel"
+  },
+  {
+    bell: "c6",
+    url: "bell72.wav?v=1597802700559",
+    type: "mabel"
+  },
+  {
+    bell: "d6",
+    url: "bell74.wav?v=1597802720100",
+    type: "mabel"
+  },
+  
+  //from Vancouver tower
+  {
+    bell: "f4",
+    url: "F4.wav?v=1602701202199",
+    type: "tower"
+  },
+  {
+    bell: "g4",
+    url: "G4.wav",
+    type: "tower"
+  },
+  {
+    bell: "a4",
+    url: "A4.wav",
+    type: "tower"
+  },
+  {
+    bell: "b4",
+    url: "Bf4.wav",
+    type: "tower"
+  },
+  {
+    bell: "c5",
+    url: "C5.wav",
+    type: "tower"
+  },
+  {
+    bell: "d5",
+    url: "D5.wav",
+    type: "tower"
+  },
+  {
+    bell: "e5",
+    url: "E5.wav",
+    type: "tower"
+  },
+  {
+    bell: "f5",
+    url: "F5.wav",
+    type: "tower"
+  },
+  {
+    bell: "g5",
+    url: "G5.wav",
+    type: "tower"
+  },
+  {
+    bell: "a5",
+    url: "A5.wav",
+    type: "tower"
+  },
+  {
+    bell: "b5",
+    url: "Bf5.wav",
+    type: "tower"
+  },
+  {
+    bell: "c6",
+    url: "C6.wav",
+    type: "tower"
+  }
+]
+
